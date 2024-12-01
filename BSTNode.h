eda2338648afcda45d8c7c09eaacaf4401082819
MohamedAshik14-blip <140538289+MohamedAshik14-blip@users.noreply.github.com>
2024-12-01 15:22:32 +0000

    Implement BSTNode class with add and remove methods
    Created a BSTNode class for binary search tree (BST) nodes, including methods for adding and removing items.The add method places new nodes according to BST rules, while the remove method handles node deletion, including cases with zero, one, or two children.

---
+++ BSTNode.h
@@ -0,0 +1,64 @@
+#pragma once
+#include <iostream>
+
+template <class T>
+class BSTNode {
+private:
+    T item;
+    BSTNode* left;
+    BSTNode* right;
+
+public:
+    BSTNode(const T& newItem)
+            : item(newItem), left(nullptr), right(nullptr) {}
+
+    T& getItem() { return item; }
+    const T& getItem() const { return item; }
+    void setItem(const T& newItem) { item = newItem; }
+
+    BSTNode* getLeft() const { return left; }
+    void setLeft(BSTNode* newLeft) { left = newLeft; }
+
+    BSTNode* getRight() const { return right; }
+    void setRight(BSTNode* newRight) { right = newRight; }
+
+    void add(const T& newItem) {
+        if (newItem < item) {
+            if (left == nullptr) {
+                left = new BSTNode(newItem);
+            } else {
+                left->add(newItem);
+            }
+        } else if (newItem > item) {
+            if (right == nullptr) {
+                right = new BSTNode(newItem);
+            } else {
+                right->add(newItem);
+            }
+        }
+    }
+
+    bool remove(const T& newItem) {
+        if (newItem < item) {
+            return left != nullptr && left->remove(newItem);
+        } else if (newItem > item) {
+            return right != nullptr && right->remove(newItem);
+        } else { 
+          
+            if (left == nullptr && right == nullptr) {
+                delete this;
+                return true;
+            }
+            if (left == nullptr || right == nullptr) {
+                BSTNode* child = left ? left : right;
+                item = child->item;
+                left = child->left;
+                right = child->right;
+                delete child;
+                return true;
+            }
+        }
+        return false;
+    }
+
+};
