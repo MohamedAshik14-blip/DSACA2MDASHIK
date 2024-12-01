491af0088b4ae3ecc31a98b08f4184e86fcd025e
MohamedAshik14-blip <140538289+MohamedAshik14-blip@users.noreply.github.com>
2024-12-01 14:56:14 +0000

    Initial CLion project setup along with googletest for unit testing

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /Users/mohamedashiks/CLionProjects/DSACA21
+# Build directory: /Users/mohamedashiks/CLionProjects/DSACA21/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[MyTests]=] "/Users/mohamedashiks/CLionProjects/DSACA21/cmake-build-debug/runTests")
+set_tests_properties([=[MyTests]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/mohamedashiks/CLionProjects/DSACA21/CMakeLists.txt;29;add_test;/Users/mohamedashiks/CLionProjects/DSACA21/CMakeLists.txt;0;")
+subdirs("googletest")
